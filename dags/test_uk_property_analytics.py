436780e2690dc7d3658b7b236c7a7664d3ea2aca
stephenpir <61986559+stephenpir@users.noreply.github.com>
2026-07-25 09:10:46 +0100

    Add Airflow Land Registry ingestion DAG

--- dags/test_uk_property_analytics.py
+++ dags/test_uk_property_analytics.py
@@ -2,21 +2,40 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 
 
 with DAG(
-    dag_id="uk_property_analytics_docker_test",
+    dag_id="uk_property_analytics_ingestion",
     start_date=datetime(2026, 1, 1),
     schedule=None,
     catchup=False,
-    tags=["uk-property-analytics", "infrastructure-test"],
+    tags=["uk-property-analytics", "ingestion"],
 ) as dag:
 
-    test_project_container = DockerOperator(
-        task_id="test_project_container",
+    ingest_land_registry = DockerOperator(
+        task_id="ingest_land_registry",
         image="uk-property-analytics:latest",
-        command="dbt --version",
-        auto_remove="success",
+        command=[
+            "python",
+            "python/ingestion/land_registry_ingest.py",
+        ],
         docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",
+        network_mode="host",
+        auto_remove="success",
+        mount_tmp_dir=False,
+        environment={
+            "AWS_REGION": "eu-west-2",
+            "S3_BUCKET": "spir23-uk-residential-property-analytics-dev",
+            "CSV_URL": "https://price-paid-data.publicdata.landregistry.gov.uk/pp-2025.csv",
+            "S3_KEY": "landing/land-registry/annual/2025/pp-2025.csv",
+        },
+        mounts=[
+            Mount(
+                source="/Users/stephenpir/.aws",
+                target="/root/.aws",
+                type="bind",
+                read_only=True,
+            ),
+        ],
     )
